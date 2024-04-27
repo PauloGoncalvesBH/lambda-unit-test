46fec9a2004cd1691e9dcfc3c1332f82c10e2470
Paulo Gonçalves <29241659+PauloGoncalvesBH@users.noreply.github.com>
2024-04-27 15:26:28 +0100

    pequeno ajuste de formatação em comentário

--- src/authorization.js
+++ src/authorization.js
@@ -1,6 +1,6 @@
 
 // Essa função é o único trecho de código que é possível validar sem o uso da biblioteca 'lambda-tester'.
-// Isso ocorre no arquivo ./tests/functions/tokenIsValid.spec.js
+//  Isso ocorre no arquivo ./tests/functions/tokenIsValid.spec.js
 
 // Para validar a integração desse método com o restante do código foram criados testes em /tests/handler
 
