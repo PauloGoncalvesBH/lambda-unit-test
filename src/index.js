bfba4abbee4f6760db4cc955997dbf5532454672
Paulo Gonçalves <29241659+PauloGoncalvesBH@users.noreply.github.com>
2024-04-27 15:26:28 +0100

    criar a documentação do repositório e pequenos ajustes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const { tokenIsValid } = require('./authentication')
+const { tokenIsValid } = require('./authorization')
 
 // Essa pequena aplicação possui apenas a lógica de validar se o usuário enviou
-// um token válido na query string ao chamar o evento.
+//  um token válido na query string ao chamar o evento.
 
-// Se o token for inválido o usuário receberá a informação que o acesso não foi autorizado.
-// Tendo sucesso na autenticação receberá a informação que foi executado com sucesso.
-// Qualquer falha que ocorrer na aplicação será levada ao 'catch', retornando mensagem do que ocorreu para o usuário.
+// Se o token for inválido o usuário receberá a informação que o acesso não foi autorizado (linhas 14 a 17).
+//  Tendo sucesso na autenticação receberá a informação que foi executado com sucesso (linhas 23 a 26).
+//  Qualquer falha que ocorrer na aplicação será levada ao 'catch', retornando mensagem do que ocorreu para o usuário (linhas 27 a 35).
 
 module.exports.handler = async (event, context, callback) => {
   try {
--- tests/functions/tokenIsValid.spec.js
+++ tests/functions/tokenIsValid.spec.js
@@ -1,12 +1,15 @@
-const { tokenIsValid } = require('../../src/authentication')
+const { tokenIsValid } = require('../../src/authorization')
 
 // Teste unitário validando o comportamento da função 'tokenIsValid'.
-// Essas validações são simples, porém pegam todo o comportamento possível para
-// esse método.
+//  Essas validações são de certa forma simples, porém pegam todo o comportamento possível para
+//  esse método.
 
 // Embora com esse teste é possível ter certeza de como essa função lida com os dados enviados,
-// não conseguimos saber se ele barraria de maneira correta as tentativas de acesso e permitiria
-// apenas o que encaixa na regra dessa função.
+//  não conseguimos saber se ele barraria de maneira correta as tentativas de acesso e permitiria
+//  apenas o que encaixa na regra dessa função.
+
+// É devido a essa incerteza do comportamento de todo o código integrado é que os testes
+//  no diretório /tests/handler fazem sentido
 
 describe('tokenIsValid', () => {
   it('Should return false when token isn\'t valid', async function () {
